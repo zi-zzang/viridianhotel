8297ddb0bc46e5344c7a1f8649e467d35701ca6c
zi-zzang <kjhwlgusdl@gmail.com>
2022-07-08 15:35:08 +0900

    header상단고정

--- script/main.js
+++ script/main.js
@@ -6,6 +6,33 @@ $(function () {
     $('.navbox').animate({ left: -100 + '%' });
   });
 
+  //세부정보 모달창
+  $('.detail p').click(function () {
+    $('.detailbox').fadeIn();
+  });
+  $('.roominfo button.close').click(function () {
+    $('.detailbox').fadeOut();
+  });
+
+  //비교함 클릭
+  $('i.heart').click(function () {
+    $(this).toggleClass('active');
+  });
+
+  $('.heart').click(function () {
+    if ($('.heart').hasClass('active')) {
+      $('.favorite')
+        .animate({ bottom: 0 + '%' })
+        .delay(3000)
+        .animate({ bottom: -100 + '%' }, 1000);
+    } else {
+      $('.favorite_delete')
+        .stop()
+        .animate({ bottom: 0 + '%' })
+        .delay(3000)
+        .animate({ bottom: -100 + '%' }, 1000);
+    }
+  });
   //메뉴바 스크롤 방지
   $('.navbox').css({ overflow: 'hidden', height: '100%' });
   $('.navbox').on('scroll touchmove mousewheel', function (event) {
